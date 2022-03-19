ab8dd2af6b83e9e39c3980e07bef4b0610798f79
omersuve <omer@shopihq.com>
2022-03-19 17:08:39 +0100

    part

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Hero from './components/Hero';
+import Gallery from './components/Gallery';
+import About from './components/About';
+import Roadmap from './components/Roadmap';
+import Teams from './components/Teams';
+import Token from './components/Token';
+import Faq from './components/Faq';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='Main-header'>
+        <Navbar />
+        <Hero />
+      </header>
+      <About />
+      <Roadmap />
+      <Gallery />
+      <Token />
+      <Teams />
+      <Faq />
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={<div></div>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
